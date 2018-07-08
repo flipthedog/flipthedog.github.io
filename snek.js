77f55364bade2d433b01422474d2089b21ba9827
Floris <florisvanrossum@gmail.com>
2018-07-08 00:27:40 -0400

    finally created the canvas?

--- snek.js
+++ snek.js
@@ -2,7 +2,7 @@
 // document.getElementById("variable").innerText = "Hello JS!";
 
 var canvas = document.getElementById("snekCanvas");
-var ctx =  canvas.getContext();
+var ctx =  canvas.getContext("2d");
 
 ctx.fillStyle = '#FF0000';
 ctx.fillRect(0,0,150,75);
