3a664716c5cce52a6e478847596db466a1a43f59
Floris <florisvanrossum@gmail.com>
2018-07-08 00:19:17 -0400

    Created the canvas

--- snek.js
+++ snek.js
@@ -1,2 +1,8 @@
 document.write(Date());
-document.getElementById("variable").innerHTML = "Hello JS!";
+document.getElementById("variable").innerText = "Hello JS!";
+
+var canvas = document.getElementById("snekCanvas");
+var ctx =  canvas.getContext();
+
+ctx.fillStyle = '#FF0000';
+ctx.fillRect(0,0,150,75);
