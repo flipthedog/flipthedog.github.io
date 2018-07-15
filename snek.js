5de09bedcbc9fb344192cbccbfb7d1400ef296e7
Floris van Rossum <florisvanrossum@gmail.com>
2018-07-15 01:37:29 -0400

    working grid

--- snek.js
+++ snek.js
@@ -15,30 +15,70 @@ var heightNumber = 15;
 var cellGap = 3;
 
 // Cell dimensions
-var cellWidth = parseInt((canvasWidth - ((widthNumber + 1) * cellGap)) / (widthNumber - 1));
-var cellHeight = parseInt((canvasHeight - ((heightNumber + 1) * cellGap)) / (heightNumber - 1));
+var cellWidth = parseInt((canvasWidth - ((widthNumber + 1 ) * cellGap)) / (widthNumber));
+var cellHeight = parseInt((canvasHeight - ((heightNumber + 1) * cellGap)) / (heightNumber));
+
+var newCanvasWidth =  ((cellGap ) * (widthNumber + 1)) + widthNumber * cellWidth;
+var newCanvasHeight = ((cellGap ) * (heightNumber + 1)) + heightNumber * cellHeight;
+
+canvas.setAttribute('width', newCanvasWidth );
+canvas.setAttribute('height', newCanvasHeight);
+
+var cellStorage = new Array(heightNumber);
+for (var i =0; i < widthNumber; i++) {
+    cellStorage[i] = new Array(widthNumber);
+}
+
+
+console.log("This is the new canvas height: " + newCanvasHeight);
+console.log("This is the new canvas width: " + newCanvasWidth);
 
 console.log(cellWidth);
 console.log(cellHeight);
 
 // ctx.fillRect(0, 0, cellWidth, cellHeight);
 
+function Cell(x, y) {
+    this.x = x;
+    this.y = y;
+}
+
 // Create the background
-createBackground();
+createCells();
+draw();
+function createCells() {
 
-function createBackground() {
 
+
+    for (var j = 0; j < heightNumber ; j++) {
+
+        for (var i = 0; i < widthNumber ; i++) {
+
+            cell = new Cell((cellWidth * i) + cellGap  * (i + 1), (cellHeight * j) + cellGap * (j+ 1))
+            cellStorage[i][j] = cell;
+        }
+
+    }
+}
+
+function draw() {
+    drawBackground();
+}
+
+function drawBackground() {
     // Create the black background
     ctx.fillStyle = '#00000e';
-    ctx.fillRect(0,0,canvasWidth,canvasHeight);
+    ctx.fillRect(0,0,newCanvasWidth ,newCanvasHeight );
 
     // Cell color
-    ctx.fillStyle = '#07055b';
+    ctx.fillStyle = '#070599';
 
-    for (var j = 0; j <= heightNumber ; j++) {
+    for (var j = 0; j < heightNumber; j++) {
 
-        for (var i = 0; i <= widthNumber ; i++) {
-            ctx.fillRect((cellWidth * i) + cellGap , (cellHeight * j) + cellGap, cellWidth - cellGap, cellHeight - cellGap);
+        for (var i = 0; i < widthNumber; i++) {
+            cell2 = cellStorage[i][j];
+            // console.log("This is the cell2 coords: " + cell2.x + ", " + cell2.y);
+            ctx.fillRect(cell2.x, cell2.y , cellWidth, cellHeight);
         }
 
     }
