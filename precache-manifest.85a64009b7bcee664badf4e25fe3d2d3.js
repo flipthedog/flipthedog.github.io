97fccdc5ca1a91bfe0d0617cc0befa53665bd347
flipthedog <florisvanrossum@gmail.com>
2018-11-12 20:39:28 +0900

    Updates

---
+++ precache-manifest.85a64009b7bcee664badf4e25fe3d2d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/website/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "df8eefd1f2dbc5f3d248",
+    "url": "/website/static/js/runtime~main.df8eefd1.js"
+  },
+  {
+    "revision": "299bc725f9bf65df5495",
+    "url": "/website/static/js/main.299bc725.chunk.js"
+  },
+  {
+    "revision": "1a2fe23bbee4e72efb16",
+    "url": "/website/static/js/1.1a2fe23b.chunk.js"
+  },
+  {
+    "revision": "299bc725f9bf65df5495",
+    "url": "/website/static/css/main.555e3b0a.chunk.css"
+  },
+  {
+    "revision": "13cd9f4820d31fbc6e46bd1535fe5c53",
+    "url": "/website/index.html"
+  }
+];
